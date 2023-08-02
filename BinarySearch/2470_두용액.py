b959a07eac6a3df2b4325278f5ef7bb4dfd91e97
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2023-08-03 04:49:55 +0900

    2023-08-03 Binary Search 06
    2023-08-03 Binary Search 06

---
+++ BinarySearch/2470_두용액.py
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+# ===========================================
+# [success] 2nd trial: c+v
+# reference: https://bladejun.tistory.com/97
+# idea; left, right 하나씩 줄이면서 abs 최솟값 찾기 
+# ---> 이렇게 보니까 진짜 쉬운 문제였음
+
+# input 입력 받기
+n = int(input())
+solution = list(map(int, input().split(' ')))
+
+# 정렬하기
+solution.sort()
+
+# 이중포인터 설정
+left = 0
+right = n-1
+
+answer = 2e+9+1 # 기준값
+final = [] # 정답
+
+# 투포인터 진행
+while left < right:
+    s_left = solution[left]
+    s_right = solution[right]
+
+    tot = s_left + s_right
+    # 두 용액의 합이 0과 가장 가까운 용액을 정답에 담아주기
+    if abs(tot) < answer:
+        answer = abs(tot)
+        final = [s_left, s_right]
+	
+    # 두 용액의합이 0보다 작다면 왼쪽의 값을 늘려 조금 더 0에 가깝게 만들어준다
+    if tot < 0:
+        left += 1
+    # 반대로, 두 용액의 합이 0보다 크다면 오른쪽의 값을 줄여서 조금 더 0에 가깝게 만들어준다
+    else:
+        right -= 1
+
+print(final[0], final[1])
+
+
+
+# ===========================================
+# [fail] 1st trial: 
+# 하나 고정하고 binary search로 다른 하나 찾으려 시도
+# 그러나 num에 가장 가까운 liquid[i]를 찾는 코드 구현 실패
+# --> 잘 생각하면 나중에 구현할 수 있을 것 같음
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# liquid = list(map(int, input().split()))
+# liquid = sorted(liquid)
+
+# start = 0
+# end = len(liquid)
+# answer = [1000000000, 10000000000]
+
+# for num in liquid:
+#     while start < end:
+#         mid = (start+end)//2
+#         if abs(num + liquid[mid]) >= sum(answer):
+
+
+#     if abs(num + liquid[end]) < sum(answer):
+#         answer = [num, end]
+
+# print(answer[0], answer[1])
