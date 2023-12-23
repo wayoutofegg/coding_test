652954353613e80d37983ec77f8997b20fc8339a
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2023-12-24 06:56:14 +0900

    Update 10773_제로.py

--- Data_Structure/10773_제로.py
+++ Data_Structure/10773_제로.py
@@ -1,16 +1,31 @@
-#2023-07-30 Data Structure 04
-
+# 2023.12.24. Correct
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-written = []
+K = int(input())
+correct_num = []
 
-for i in range(n):
-    number = int(input())
-    if number == 0:
-        written.pop()
+for i in range(K):
+    temp = int(input())
+    if temp != 0:
+        correct_num.append(temp)
     else:
-        written.append(number)
+        correct_num.pop()
+        
+print(sum(correct_num))
 
-print(sum(written))
+#2023-07-30 Data Structure 04
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# written = []
+
+# for i in range(n):
+#     number = int(input())
+#     if number == 0:
+#         written.pop()
+#     else:
+#         written.append(number)
+
+# print(sum(written))
