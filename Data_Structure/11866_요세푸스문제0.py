e33066f1db43ad9e946fd73c431d0b960cde11ec
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2023-12-25 06:36:41 +0900

    Update 11866_요세푸스문제0.py

--- Data_Structure/11866_요세푸스문제0.py
+++ Data_Structure/11866_요세푸스문제0.py
@@ -1,27 +1,43 @@
-# 2023-07-30 Data Structure 12
-# 1st Trial:
-
+# 2023-12-25 Correct
+# 1. Deck으로 구현하면 훨씬 쉬움
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-PeopleinCircle, EliminatedUnit = map(int, input().split())
-RemainedPeople = [i for i in range(1, PeopleinCircle+1)]
-EliminatedPeople = []
+answer = []
+N, K = map(int, input().split())
+people = deque([i for i in range(1, N+1)])
 
-PersonCount = 0
-for _ in range(PeopleinCircle):
-    PersonCount += EliminatedUnit - 1 
-    if PersonCount >= len(RemainedPeople):
-        PersonCount = PersonCount%len(RemainedPeople)
-    EliminatedPeople.append(RemainedPeople[PersonCount])
-    del RemainedPeople[PersonCount]
+for i in range(N):
+    for j in range(K-1):
+        people.append(people.popleft())
+    answer.append(people.popleft())
 
-print('<',end='')
-for EachPerson in range(len(EliminatedPeople)):
-    if EachPerson == len(EliminatedPeople)-1:
-        print(EliminatedPeople[EachPerson], end='')
-    else:
-        print(EliminatedPeople[EachPerson], end=', ')
-print('>',end='')
+print("<" + ', '.join([str(i) for i in answer]) + '>')
+
+# 2023-07-30 Data Structure 12
+# 1st Trial:
+# import sys
+# input = sys.stdin.readline
+
+# PeopleinCircle, EliminatedUnit = map(int, input().split())
+# RemainedPeople = [i for i in range(1, PeopleinCircle+1)]
+# EliminatedPeople = []
+
+# PersonCount = 0
+# for _ in range(PeopleinCircle):
+#     PersonCount += EliminatedUnit - 1 
+#     if PersonCount >= len(RemainedPeople):
+#         PersonCount = PersonCount%len(RemainedPeople)
+#     EliminatedPeople.append(RemainedPeople[PersonCount])
+#     del RemainedPeople[PersonCount]
+
+# print('<',end='')
+# for EachPerson in range(len(EliminatedPeople)):
+#     if EachPerson == len(EliminatedPeople)-1:
+#         print(EliminatedPeople[EachPerson], end='')
+#     else:
+#         print(EliminatedPeople[EachPerson], end=', ')
+# print('>',end='')
 
 # 이거 방법 Review: print('<',', '.join(map(str, EliminatedPeople[:])),'>',sep='')
