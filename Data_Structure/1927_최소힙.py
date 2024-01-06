eff1a9eff209206ed7bc449eccde9618dd6488ec
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2024-01-06 21:53:35 +0900

    Update 1927_최소힙.py

--- Data_Structure/1927_최소힙.py
+++ Data_Structure/1927_최소힙.py
@@ -1,3 +1,6 @@
+# 2024-01-06 PASS
+#     you need to be familiar with "heapq" library
+
 import sys
 import heapq
 input = sys.stdin.readline
@@ -8,9 +11,7 @@ Array = []
 for Command in range(NumberOfCommands):
     Command = int(input())
     if Command == 0:
-        if Array:
-            print(heapq.heappop(Array))
-        else:
-            print(0)
+        if Array: print(heapq.heappop(Array))
+        else: print(0)
     else:
         heapq.heappush(Array, Command)
