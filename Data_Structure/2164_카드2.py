7b8a1f779a75d52b6c6c5d67ae861f192ee9a252
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2024-01-07 16:18:28 +0900

    Update 2164_카드2.py

--- Data_Structure/2164_카드2.py
+++ Data_Structure/2164_카드2.py
@@ -1,34 +1,51 @@
-# 2023-07-30 Data Structure 06
-
-import time
-start = time.time()
-
+# 2024-01-06 PASS
+# Here it is, simpler version
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-cards = []
-for i in range(1,n+1):
-    cards.append(i)
+N = int(input())
+cards = deque([i for i in range(1, N+1)])
 
-while len(cards) > 1:
-    if len(cards)%2==0:
-        temp = []
-        for i in range(len(cards)//2):
-            temp.append(cards[i*2+1])
-        cards = temp
+while True:
+    if len(cards) == 1:
+        print(cards.pop())
+        break
     else:
-        temp = [cards[-1]]
-        del cards[-1]
-        for i in range(len(cards)//2):
-            temp.append(cards[i*2+1])
-        cards = temp
+        cards.popleft()
+        cards.append(cards.popleft())
 
-    # del cards[0]
-    # cards.append(cards[0])
-    # del cards[0] #-------------------> 시간초과
+# 2023-07-30 Data Structure 06
 
-print(cards[0])
+# import time
+# start = time.time()
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# cards = []
+# for i in range(1,n+1):
+#     cards.append(i)
+
+# while len(cards) > 1:
+#     if len(cards)%2==0:
+#         temp = []
+#         for i in range(len(cards)//2):
+#             temp.append(cards[i*2+1])
+#         cards = temp
+#     else:
+#         temp = [cards[-1]]
+#         del cards[-1]
+#         for i in range(len(cards)//2):
+#             temp.append(cards[i*2+1])
+#         cards = temp
+
+#     # del cards[0]
+#     # cards.append(cards[0])
+#     # del cards[0] #-------------------> 시간초과
+
+# print(cards[0])
 # print(time.time()-start)
 
 
