5f658efe2db1ed3224b6440a5c38f4c07ec50a4a
wayoutofegg <138763149+wayoutofegg@users.noreply.github.com>
2024-01-10 19:27:10 +0900

    Update 1049_기타줄.py

--- Greedy/1049_기타줄.py
+++ Greedy/1049_기타줄.py
@@ -1,3 +1,25 @@
+# 2024-01-10 FAIL
+# 1. 전선 가격은 브랜드별로 다를 뿐
+# 2. 최소값 후보 다 골라서 한 번에 최소값 출력하면 될 뿐
+# 3. 6개짜리가 엄청 싸서, 이것만 사는 게 이득인 경우도 가능하다!
+
+import sys
+input = sys.stdin.readline
+N, brands = map(int, input().split())
+min_package = 10**3
+min_each = 10**3
+answer = 0
+
+for _ in range(brands):
+    package, each = map(int, input().split())
+    min_package = min(min_package, package)
+    min_each = min(min_each, each)
+    
+answer += min(min_package*(N//6), min_each*(N-N%6))
+answer += min(min_package, min_each*(N%6))
+print(answer)
+
+# -===================================================
 # 19th commit
 # 예외 고려하는 게 핵심... 너무 많이 틀렸다! 정답률 25%
 # n 끊어진 줄 개수
